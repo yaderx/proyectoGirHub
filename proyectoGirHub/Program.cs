91d4b98b10235e738ad8edb1a0d0c716073d35d5
santosjader44@gmail.com <santosjader44@gmail.com>
2025-05-06 14:49:03 -0600

    Mensaje principal modificado

--- proyectoGirHub/Program.cs
+++ proyectoGirHub/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Mi primer proyecto de Github");
         }
     }
 }
+
